45273380cfb64cc5fa24c70dca65de225477b752
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-09-29 15:27:59 +0200

    Dodanie programów z zestawu zadań

---
+++ Programy z PDF'ów z PdoM/Krok 1/Od problemu do programu/1. Od problemu do programu/celcjusz na fahrenheit.py
@@ -0,0 +1,3 @@
+temp = int(input("Podaj temperature w stopniach Celcjusza: "))
+
+print(((9/5) * temp) + 32)
---
+++ Programy z PDF'ów z PdoM/Krok 1/Od problemu do programu/1. Od problemu do programu/fahrenheit na celcjusz.py
@@ -0,0 +1,3 @@
+temp = int(input("Podaj temperature w stopniach Fahrenheita: "))
+
+print(round((temp - 32) * (5/9), 2))
