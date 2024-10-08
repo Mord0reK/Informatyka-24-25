f977603e5a8282aed883114419de13ac6145fd8a
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-10-08 18:27:17 +0200

    Dodanie programów z zestawu zadań

--- Programy z PDF'ów z PdoM/Krok 1/Od problemu do programu/4. Wprowadzenie do języka c++/18. Tablica dwuwymiarowa 4x5 przerzucanie wierszy.py
+++ Programy z PDF'ów z PdoM/Krok 1/Od problemu do programu/4. Wprowadzenie do języka c++/18. Tablica dwuwymiarowa 4x5 przerzucanie wierszy.py
@@ -4,3 +4,14 @@ tablica = [
     [11, 12, 13, 14, 15],
     [16, 17, 18, 19, 20]
 ]
+
+wiersz = int(input("Podaj numer wiersza, który chcesz usunąć (1,4): "))
+
+wiersz -= 1
+
+tablica.pop(wiersz)
+
+for i in range(3):
+    for j in range(4):
+        print(tablica[i][j], end=" ")
+    print()
