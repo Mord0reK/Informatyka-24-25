ed5604927968ecddbc68bb1c5354c31dea49a190
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-10-15 17:34:19 +0200

    Dodanie programów z zestawu zadań (1.2)

--- Zestawy zadań z PdoM/Krok 1/Systemy liczbowe i reprezentacja danych w komputerze/Zestaw 1/15. Czy nie da sie stworzyc trojkata.py
+++ Zestawy zadań z PdoM/Krok 1/Systemy liczbowe i reprezentacja danych w komputerze/Zestaw 1/15. Czy nie da sie stworzyc trojkata.py
@@ -1,29 +1,29 @@
-def check_no_triangle(nums):
-    n = len(nums)
+def trojkat(a, b, c):
+    return not ((a + b > c) and (a + c > b) and (b + c > a))
 
-    # Sprawdzenie wszystkich trójek liczb
+n = int(input("Podaj liczbę liczb (n): "))
+
+if n < 3:
+    print("Musi być przynajmniej 3 liczby.")
+else:
+    liczby = []
+    for i in range(n):
+        liczba = int(input(f"Podaj liczbę {i + 1}: "))
+        liczby.append(liczba)
+
+    znaleziono_niezgodnosc = False
     for i in range(n):
         for j in range(i + 1, n):
             for k in range(j + 1, n):
-                x, y, z = nums[i], nums[j], nums[k]
+                if trojkat(liczby[i], liczby[j], liczby[k]):
+                    znaleziono_niezgodnosc = True
+                    break
+            if znaleziono_niezgodnosc:
+                break
+        if znaleziono_niezgodnosc:
+            break
 
-                # Sprawdzamy, czy da się zbudować trójkąt
-                if x + y <= z or x + z <= y or y + z <= x:
-                    return True  # Znaleziono trójkę, która nie spełnia warunku
-
-    return False  # Wszystkie trójki spełniają warunek
-
-
-# Wczytanie liczb od użytkownika
-n = int(input("Podaj liczbę elementów: "))
-nums = []
-
-for _ in range(n):
-    num = int(input("Podaj liczbę: "))
-    nums.append(num)
-
-# Sprawdzenie, czy istnieje trójka, z której nie da się zbudować trójkąta
-if check_no_triangle(nums):
-    print("Istnieje trójka liczb, dla której nie można zbudować trójkąta.")
-else:
-    print("Nie ma takiej trójki liczb.")
+    if znaleziono_niezgodnosc:
+        print("TAK")
+    else:
+        print("NIE")
